1e9c4cdeb5758f7c7782e233332baf362a84fa44
Diyana Fernando <diyana@incubatelabs.com>
2023-09-28 18:38:21 +0530

    navbar

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'btn-pink': '#EF5DA8',
+      },
+    },
+    screens: {
+      '3xs': '320px',
+      '2xs': '420px',
+      xs: '520px',
+      sm: '640px',
+      md: '768px',
+      '2md': '920px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+      '3xl': '1920px',
+    },
+  },
+  plugins: [],
+};
