6d19ab1848ec190b02992aacf7d2401e4ec3259b
Diyana Fernando <diyana@incubatelabs.com>
2023-09-30 16:12:35 +0530

    completed the card section

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,17 @@ import './App.css';
 import NavBar from './Components/NavBar';
 import circles from './assets/circles.png';
 import Hero from './Components/Hero';
+// import CardItems from './Components/Cards/CardItems';
+import Card from './Components/Cards/Cards';
+import cardData from './Components/Cards/CardData';
 
 function App() {
   return (
-    <div className="container">
+    <div>
       {/* <img src={circles}/> */}
       <NavBar/>
       <Hero/>
+      <Card data={cardData} />
  
     </div>
   );
